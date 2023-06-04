8882becd8b8dbb3eeb3ff39459182b698a027b95
Tsuri Yuma <yuma33t@gmail.com>
2023-06-04 14:01:55 +0900

    Initial Commit

---
+++ Janken/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Janken
+//
+//  Created by 釣悠馬 on 2023/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by 釣悠馬 on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
