7f203894ab5a572868e0eee35aa1b2e203efe50c
Tsuri Yuma <yuma33t@gmail.com>
2023-06-04 14:10:04 +0900

    2023/06/04

--- Janken/ContentView.swift
+++ Janken/ContentView.swift
@@ -1,22 +1,177 @@
 //
 //  ContentView.swift
-//  Janken
+//  JankenApp
 //
-//  Created by 釣悠馬 on 2023/06/04.
+//  Created by 釣悠馬 on 2023/04/04.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingRecordModal = false
+    @State private var playerHand = 0
+    @State private var computerHand = 0
+    @State private var text = "じゃんけん・・・"
+    @AppStorage("winningStreak") var winningStreak = 0
+    @AppStorage("numMatch") var numMatch = 0
+    @AppStorage("numWin") var numWin = 0
+    @AppStorage("numLose") var numLose = 0
+    @AppStorage("numAiko") var numAiko = 0
+    @AppStorage("winRate") var winRate = 0
+    @AppStorage("mostWinningStreak") var mostWinningStreak = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .topTrailing) {
+            Button(action: {
+                self.showingRecordModal.toggle()
+            }){
+                Image(systemName: "line.3.horizontal.circle")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .frame(width: 30,height: 30)
+            }
+            .sheet(isPresented: $showingRecordModal) {
+                RecordView(winningStreak: self.$winningStreak, numMatch: self.$numMatch, numWin: self.$numWin, numLose: self.$numLose, numAiko: self.$numAiko, winRate: self.$winRate, mostWinningStreak: self.$mostWinningStreak)
+            }.padding()
+            
+            VStack {
+                Image("oldman_face")
+                    .resizable()
+                    .scaledToFit()
+                //            相手の手
+                if (computerHand == 0) {
+                    Image("janken_gu")
+                        .resizable()
+                        .scaledToFit()
+                        .rotationEffect(Angle(degrees: 180))
+                } else if (computerHand == 1) {
+                    Image("janken_choki")
+                        .resizable()
+                        .scaledToFit()
+                        .rotationEffect(Angle(degrees: 180))
+                } else if (computerHand == 2) {
+                    Image("janken_pa")
+                        .resizable()
+                        .scaledToFit()
+                        .rotationEffect(Angle(degrees: 180))
+                }
+                
+                Text(text)
+                    .font(.title)
+                Text("\(winningStreak)連勝中！")
+                    .font(.title)
+                
+                //            自分の手
+                if (playerHand == 0) {
+                    Image("janken_gu")
+                        .resizable()
+                        .scaledToFit()
+                } else if (playerHand == 1) {
+                    Image("janken_choki")
+                        .resizable()
+                        .scaledToFit()
+                } else if (playerHand == 2) {
+                    Image("janken_pa")
+                        .resizable()
+                        .scaledToFit()
+                }
+                //            ボタン
+                HStack {
+                    Button(action: {
+                        playerHand = 0
+                        self.computerHand = chooseComputerHand()
+                        self.text = determineVictoryOrDefeat(playerHand: self.playerHand, computerHand: self.computerHand)
+                        storeRecord(playerHand: self.playerHand, computerHand: self.computerHand)
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//                            self.text = "じゃんけん・・・"
+//                        }
+                    }) {
+                        Image("janken_gu_tab")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    Button(action: {
+                        playerHand = 1
+                        self.computerHand = chooseComputerHand()
+                        self.text = determineVictoryOrDefeat(playerHand: self.playerHand, computerHand: self.computerHand)
+                        storeRecord(playerHand: self.playerHand, computerHand: self.computerHand)
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//                            self.text = "じゃんけん・・・"
+//                        }
+                    }) {
+                        Image("janken_choki_tab")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    Button(action: {
+                        playerHand = 2
+                        self.computerHand = chooseComputerHand()
+                        self.text = determineVictoryOrDefeat(playerHand: self.playerHand, computerHand: self.computerHand)
+                        storeRecord(playerHand: self.playerHand, computerHand: self.computerHand)
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//                            self.text = "じゃんけん・・・"
+//                        }
+                    }) {
+                        Image("janken_pa_tab")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+            }
         }
-        .padding()
     }
+}
+
+func chooseComputerHand() -> Int {
+    let random = Int.random(in: 0..<3)
+    let computerHand = random
+    return computerHand
+}
+
+func determineVictoryOrDefeat(playerHand:Int, computerHand:Int) -> String {
+    var result = ""
+    let playerHandTemp = playerHand + 1
+    
+    if (playerHand == computerHand) {
+        result = "ぽん...あいこです"
+    } else if ((playerHand == 2 && computerHand == 0) || (playerHandTemp == computerHand)) {
+        result = "ぽん...あなたの勝ちです"
+    } else {
+        result = "ぽん...あなたの負けです"
+    }
+    return result
+}
+
+func storeRecord(playerHand:Int, computerHand:Int) {
+    let playerHandTemp = playerHand + 1
+    @AppStorage("winningStreak") var winningStreak = 0
+    @AppStorage("numMatch") var numMatch = 0
+    @AppStorage("numWin") var numWin = 0
+    @AppStorage("numLose") var numLose = 0
+    @AppStorage("numAiko") var numAiko = 0
+    @AppStorage("winRate") var winRate = 0
+    @AppStorage("mostWinningStreak") var mostWinningStreak = 0
+    
+//    あいこ
+    if (playerHand == computerHand) {
+        winningStreak = 0
+        numMatch += 1
+        numAiko += 1
+//    勝ち
+    } else if ((playerHand == 2 && computerHand == 0) || (playerHandTemp == computerHand)) {
+        winningStreak += 1
+        numMatch += 1
+        numWin += 1
+        if (winningStreak > mostWinningStreak) {
+            mostWinningStreak = winningStreak
+        }
+//    負け
+    } else {
+        winningStreak = 0
+        numMatch += 1
+        numLose += 1
+    }
+    winRate = numWin * 100 / numMatch
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Janken/RecordView.swift
@@ -0,0 +1,48 @@
+//
+//  RecordView.swift
+//  JankenApp
+//
+//  Created by 釣悠馬 on 2023/04/30.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    
+    @Binding var winningStreak: Int
+    @Binding var numMatch: Int
+    @Binding var numWin: Int
+    @Binding var numLose: Int
+    @Binding var numAiko: Int
+    @Binding var winRate: Int
+    @Binding var mostWinningStreak: Int
+    
+    var body: some View {
+        VStack {
+            Text("戦績").font(.largeTitle)
+            Text("対戦数：\(numMatch)")
+            Text("勝利数：\(numWin)")
+            Text("敗北数：\(numLose)")
+            Text("あいこ数：\(numAiko)")
+            Text("勝率：\(winRate)%")
+            Text("現在の連勝数：\(winningStreak)")
+            Text("過去最高連勝数：\(mostWinningStreak)")
+            Button(action: {
+                UserDefaults.standard.removeObject(forKey: "numMatch")
+                UserDefaults.standard.removeObject(forKey: "numWin")
+                UserDefaults.standard.removeObject(forKey: "numLose")
+                UserDefaults.standard.removeObject(forKey: "numAiko")
+                UserDefaults.standard.removeObject(forKey: "winRate")
+                UserDefaults.standard.removeObject(forKey: "mostWinningStreak")
+            }) {
+                Text("リセット")
+            }
+        }.font(.title)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView(winningStreak: .constant(0), numMatch: .constant(0), numWin: .constant(0), numLose: .constant(0), numAiko: .constant(0), winRate: .constant(0), mostWinningStreak: .constant(0))
+    }
+}
